a7e20c8c826238cbabafb7c46651a761eccb2d83
TamirlanNurbulatuly <t.nurbulatuly@gmail.com>
2023-03-11 02:14:26 +0600

    добавили продукты с Каспи и при заказе редайрект в телеграмм

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,13 +5,15 @@ import { products } from '../products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products = [...products];
 
-  share() {
-    window.alert('The product has been shared!');
+  share(url : string) {
+    var otvet = window.prompt()
+    var link = "https://t.me/share/url?url="+url+"&text=" + otvet;
+    window.open(link);
   }
 
   onNotify() {
